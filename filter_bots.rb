67c5d6b2137249112b337473065a365c9aa5b58b
jwmount <john@venuesoftware.com>
2009-07-02 12:25:43 -0700

    Modified filter_bots.rb to count blank UserAgent fields as bots.

--- filter_bots.rb
+++ filter_bots.rb
@@ -1,9 +1,10 @@
 # #!/usr/local/bin/ruby
 #                    
 #filter_bots.rb
+#(c) Copyright 2009 VenueSoftware Corp. All Rights Reserved.
 require 'QClass.rb'
 # Bot list from Intelius, downcased for case insensitivity
-BOT_TOKENS = %w[ '' bot mediapartners-google inktomi slurp turnitin ask.com yahoo transcoder hotjobs voyager lwp-trivial mail.ru teoma 
+BOT_TOKENS = %w[ bot mediapartners-google inktomi slurp turnitin ask.com yahoo transcoder hotjobs voyager lwp-trivial mail.ru teoma 
                crawler findlinks twiceler ia_archiver-web nutchcvs wwwster spider sbider winhttprequest rpt-httpclient ichiro cfnetwork nagios 
                lwp::simple libwww python-urllib wget ]
 VAC_bot_tokens = %w[ nil bot crawl seek scan search dig agent get spider scooter lint libwww loader mechanic curl link catch fly ]
@@ -11,6 +12,7 @@ class Qry < QClass
   def initialize( file_in, out_file, limit)
     super( file_in, out_file, limit)
     @bots = 0
+    @blank_bots = 0
     @isbot = false
     filter
   end
@@ -23,6 +25,11 @@ class Qry < QClass
         limit?
         progress?
         fields = line.split("\t")
+        if fields[UA_ID].length == 0
+          @bots += 1
+          @blank_bots += 1
+          next
+        end
         next if fields.length < UA_ID
         fields[UA_ID].downcase!
         @isbot = false
@@ -38,6 +45,7 @@ class Qry < QClass
     end #file block
 
   wrap_up("\nBOTS\t\t\t\t\t\t\t\t" + @bots.to_s  + "\n\n")
+  puts @blank_bots
   end
 
 end
